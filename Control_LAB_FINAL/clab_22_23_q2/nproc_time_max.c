a17bf0fd5bf250f37640c2f117afedd3b77330cf
RogerCL <90930371+RogerCL24@users.noreply.github.com>
2023-12-12 12:27:01 +0100

    Update nproc_time_max.c

--- Control_LAB_FINAL/clab_22_23_q2/nproc_time_max.c
+++ Control_LAB_FINAL/clab_22_23_q2/nproc_time_max.c
@@ -15,32 +15,34 @@ void Usage() {
 
 
 int main(int argc, int *argv[]) {
-    int t, max_time = 0;
-    char buff[100];
+    int max_time = 0, fd1, fd2, t, ret;
+    char buff[100], c, number[100];
     if (argc < 2) Usage();
     int exit_stat, exit_code;
     //if (mknod("./PipeA", S_IRUSR|S_IWUSR|__S_IFIFO, 0) < 0) error(1, errno, "mknod");
-    int fd1 = open("PipeA", O_WRONLY);
-    int fd2 = open("PipeA" ,O_RDONLY);
-    if (fd1 < 0 || fd2 < 0) error(1, errno, "open");
-
-    for (int i = 1; i < argc; ++i) {
+   
+    for (int i = 1; i < argc; ++i) { 
+        int p = 0;
         int pid = fork();
         if (pid < 0) error(1,errno, "fork");
         else if (pid == 0) {
-            dup2(fd1, 1);
-            close(fd1); close(fd2);
+            close(1);
+            if ((fd2 = open("PipeA" ,O_WRONLY)) < 0) error(1, errno, "open write");
+            close(fd2);                                                     //close write pipe
             execl("proc_time","./proc_time", argv[i], NULL);
             error(1, errno, "execlp");
         }
         else {
-            close(fd1);
-            if (read(fd2, &t, sizeof(int))< 0) error(1, errno, "read");
-            close(fd2);
+            if ((fd1 = open("PipeA", O_RDONLY)) < 0) error(1, errno, "open read");
+            while ((ret = read(fd1, &c, sizeof(char))) > 0) {
+                number[p] = c;
+                number[p + 1] = ' ';
+                ++p;
+            }
+            if (ret < 0) error(1, errno, "read");
+            t = atoi(&number[0]);
             if (t > max_time) max_time = t;
-
             if (waitpid(pid, &exit_stat, 0) < 0) error(1, errno, "waitpid");
-
             if (WIFEXITED(exit_stat)) {
                 exit_code = WEXITSTATUS(exit_stat);
                 if (exit_code == 255) error(1,0,"There is a not existent PID");
