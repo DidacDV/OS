b588e744b86a38ff3d05c3b6cf06df4b76dbd97e
RogerCL <90930371+RogerCL24@users.noreply.github.com>
2023-12-13 14:49:03 +0100

    Add files via upload

--- Control_LAB_FINAL/clab_22_23_q2/nproc_time_max2.c
+++ Control_LAB_FINAL/clab_22_23_q2/nproc_time_max2.c
@@ -15,15 +15,15 @@ void Usage() {
 
 
 int main(int argc, int *argv[]) {
-    int max_time = 0, fd1, fd2, t, ret;
+    int max_time = 0, t, ret;
     char buff[100], c, number[100];
     if (argc < 2) Usage();
     int exit_stat, exit_code;
     for (int i = 1; i < argc; ++i) { 
         int p = 0;
-        int pid = fork();
         int *fd = malloc(2*sizeof(int));
         pipe(fd);
+        int pid = fork();
         if (pid < 0) error(1,errno, "fork");
         else if (pid == 0) {
             dup2(fd[1], 1);
@@ -45,14 +45,14 @@ int main(int argc, int *argv[]) {
         }
     }
     
-    while ((ret = waitpid(-1, &exit_stat, 0) > 0) { 
-     if (WIFEXITED(exit_stat)) {
-         exit_code = WEXITSTATUS(exit_stat);
-         if (exit_code == 255) error(1,0,"There is a not existent PID");
-     }
+    for (int i = 1; i < argc; ++i) {
+        if ((ret = waitpid(-1, &exit_stat, 0)) < 0) error(1, errno, "waitpid");
+        if (WIFEXITED(exit_stat)) {
+            exit_code = WEXITSTATUS(exit_stat);
+            if (exit_code == 255) error(1,0,"There is a not existent PID");
+     }  
     }
-    if (ret < 0) error(1, errno, "waitpid");
-    sprintf(buff, "Max_time: %d",max_time);
+    sprintf(buff, "Max_time: %d\n",max_time);
     if (write(1,buff, strlen(buff)) < 0) error(1, errno, "write");
 
 
