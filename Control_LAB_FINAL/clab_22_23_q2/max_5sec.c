fe678ae033059c47317eddd5680ecb815721687b
RogerCL <90930371+RogerCL24@users.noreply.github.com>
2023-12-15 09:21:58 +0100

    Update max_5sec.c

--- Control_LAB_FINAL/clab_22_23_q2/max_5sec.c
+++ Control_LAB_FINAL/clab_22_23_q2/max_5sec.c
@@ -40,7 +40,7 @@ int main(int argc, int *argv[]) {
     char buff[100], c, number[100];
     if (argc < 2) Usage();
     int exit_stat, exit_code;
-    //if (mknod("./PipeA", S_IRUSR|S_IWUSR|__S_IFIFO, 0) < 0) error(1, errno, "mknod");
+    if (mknod("./PipeA", S_IRUSR|S_IWUSR|__S_IFIFO, 0) < 0 && errno != EEXIST) error(1, errno, "mknod");
 
     if (sigaction(SIGALRM, &sa, NULL) < 0) error(1, errno, "sigaction alarm");
     
