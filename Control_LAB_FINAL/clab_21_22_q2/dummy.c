2f6620b52dcc9c8a0cf15adfff94c1bcae92b1a1
RogerCL <90930371+RogerCL24@users.noreply.github.com>
2023-12-30 20:44:42 +0100

    Usage function

--- Control_LAB_FINAL/clab_21_22_q2/dummy.c
+++ Control_LAB_FINAL/clab_21_22_q2/dummy.c
@@ -1,7 +1,14 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <error.h>
+#include <errno.h>
+
+void Usage() {
+    error(1, 0, "Invalid argument\n Correct usage: ./dummy seed_number");
+}
 
 int main(int argc, char *argv[]) {  
+    if (argc != 2) Usage();
     int seed = atoi(argv[1]);
     sleep(seed);
     srand(seed);
