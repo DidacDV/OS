1cbf72145fe7dd8d5997aa35a7a4606169c22130
RogerCL <90930371+RogerCL24@users.noreply.github.com>
2024-01-13 13:03:08 +0100

    Add files via upload

---
+++ Control_LAB_FINAL/clab_21_22_q1/dir.sh
@@ -0,0 +1,11 @@
+mkdir -p D1/D_1_1/D_1_1_1
+mkdir -p D1/D_1_1/D_1_1_2
+mkdir -p D1/D_1_2
+mkdir -p D1/D_1_3
+mkdir -p D1/D_1_4
+mkdir -p D1/D_1_5
+mkdir -p D1/D_1_6
+mkdir -p D1/D_1_7
+mkdir -p D1/D_1_8
+
+ls -la D1
